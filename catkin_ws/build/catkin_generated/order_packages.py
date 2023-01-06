0cf258f31c883eb3dd8c4af54b8f0d3625ae0b0a
yutingk <b0721242@cgu.edu.tw>
2023-01-07 02:12:01 +0800

    upload sensor_tower urdf

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/julie/mmwave_realsense_mapping/catkin_ws/src'
+whitelisted_packages = 'sensor_tower'.split(';') if 'sensor_tower' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
---
+++ catkin_ws/build/sensor_tower/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/julie/mmwave_realsense_mapping/catkin_ws/src/sensor_tower
+# Build directory: /home/julie/mmwave_realsense_mapping/catkin_ws/build/sensor_tower
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
