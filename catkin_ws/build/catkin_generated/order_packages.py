78ece59ba037463b993c6b9a6506e0272e2e50d0
yutingk <b0721242@cgu.edu.tw>
2023-01-08 15:09:24 +0800

    update

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/julie/mmwave_realsense_mapping/catkin_ws/src'
 whitelisted_packages = 'sensor_tower'.split(';') if 'sensor_tower' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/julie/mmwave_realsense_mapping/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/julie/mmwave_realsense_mapping/catkin_ws/devel;/opt/ros/noetic' != '' else []
