3127b761345f97b825346daac60d2f745ff32f7b
yutingk <98682859+yutingk@users.noreply.github.com>
2023-01-08 15:24:22 +0800

    Delete catkin_ws/build directory

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/julie/mmwave_realsense_mapping/catkin_ws/src'
-whitelisted_packages = 'sensor_tower'.split(';') if 'sensor_tower' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/julie/mmwave_realsense_mapping/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/julie/mmwave_realsense_mapping/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- catkin_ws/build/sensor_tower/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/julie/mmwave_realsense_mapping/catkin_ws/src/sensor_tower
-# Build directory: /home/julie/mmwave_realsense_mapping/catkin_ws/build/sensor_tower
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
